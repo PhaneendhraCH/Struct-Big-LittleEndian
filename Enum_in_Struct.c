9d64c6e2e32fa3e4a72bb3ce19b2d3624b5cf3cd
Phaneendhra <phaneendhra.chinta@thundersoft.com>
2021-11-14 20:43:01 +0530

    Enums within a structure

---
+++ Enum_in_Struct.c
@@ -0,0 +1,1 @@
+#if 0    Accessing Enums within a structure in C        Author : Phaneendhra#endif#include <stdio.h>#include <stdlib.h>typedef enum {a,b,v,d,e} myday; struct node{        int data;    myday day;    };int main(){    struct node *p = malloc(sizeof(struct node));    p->day = a;    printf("%d\t%d",p->data=10,p->day = d);    return 0;}
